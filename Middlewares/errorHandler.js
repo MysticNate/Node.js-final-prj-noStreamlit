06627dedded1e2f56353712bfdb249595201c14c
MysticNate <164735491+MysticNate@users.noreply.github.com>
2025-08-13 20:24:55 +0300

    Update errorHandler.js

--- Middlewares/errorHandler.js
+++ Middlewares/errorHandler.js
@@ -1,4 +1,10 @@
 export function errorHandler(err, req, res, next) {
   console.error(err.message);
+  
+  // Check if response was already sent
+  if (res.headersSent) {
+    return next(err);
+  }
+  
   res.status(500).json({ error: err.message });
 }
